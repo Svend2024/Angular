d7c2dc8e91071b2ab7a90f36cb8fcf1e0f32564a
LucasLadjevardi <lucladjevardi@gmail.com>
2024-01-25 15:10:46 +0100

    Routing samt mindre login tbm styling af login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { Routes, provideRouter, withDebugTracing } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StoreComponent } from './store/store.component';
+import { DeckComponent } from './deck/deck.component';
+import { CartComponent } from './cart/cart.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'store', component: StoreComponent },
+    { path: 'deck', component: DeckComponent },
+    { path: 'cart', component: CartComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'login', component: LoginComponent },
+];
+
+export const appConfig: ApplicationConfig = {
+    providers: [provideRouter(routes, withDebugTracing())]
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Login } from '../interfaces/login';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login : Login =  {
+    username: '',
+    password: ''
+  }
+
+  public validator = {
+    'Username': [
+      { type: 'required', message: "Username is empty"}
+    ], 
+    'Password': [
+      { type: 'required', message: "Password is empty"}
+    ]
+  }
+
+  onLogin() {
+    console.log(this.login.username + " " + this.login.password)
+  }
+
+  onCreateAccount(){
+    return
+  }
 
 }
+
